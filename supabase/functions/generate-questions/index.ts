fca1caf90ab61428c2669ae6616f3c6fe753dde5
Nargees Syed <nargees2009@gmail.com>
2025-12-26 08:53:35 +0530

    force redeploy4

--- supabase/functions/generate-questions/index.ts
+++ supabase/functions/generate-questions/index.ts
@@ -41,7 +41,7 @@ Deno.serve(async (req) => {
 
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro",
+     model: "gemini-pro",
     });
 
     const result = await model.generateContent(
